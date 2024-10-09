4a802a4b12d788e272296440b697d401f35c4599
jmcanog03 <jmcanog03@educarex.es>
2024-10-09 14:56:32 +0200

    v1.1

---
+++ 00_imprimir_en_consola_y_tipo_de_datos.py
@@ -0,0 +1,26 @@
+# Esto es un comentario:
+# Nuestro hola mundo en python
+print("Hola Python")
+print('Hola Python')
+
+"""
+Este es un
+comentario 
+en varias líneas
+"""
+
+'''
+Este tambien es un
+comentario 
+en varias líneas
+'''
+
+'Consultar el tipo de dato'
+print(type('Soy un dato str')) # tipo 'str'
+print(type(5)) # tipo 'int'
+print(type(1.5)) # tipo 'float'
+print(type(True)) # tipo 'boleano'
+print(type(1 + 2j)) # tipo 'complejo'
+
+
+
