5b56614858adf0b82137318b9a743921d8ed8458
jmcanog03 <jmcanog03@educarex.es>
2024-10-09 16:04:36 +0200

    v1.2

--- 01_variables.py
+++ 01_variables.py
@@ -6,6 +6,14 @@ print(my_variable)
 my_int_variable = 5
 print(my_int_variable)
 
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+
+
+
 my_bool_variable = True
 print(my_bool_variable)
 
+print(my_variable, my_int_variable, my_bool_variable)
